eab2cd3feadab95440cc5ad435cdae78eed1c19b
Daniel Garcia <pel_daniel@hotmail.com>
2018-10-09 23:11:18 -0500

    Add Hot Module Replacement in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   devtool: 'inline-source-map',
+  mode: 'development',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
